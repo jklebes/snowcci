0d8e9d93e56a8d71a6d5a24465405d20fb4cb9ad
jklebes <jason.klebes@ed.ac.uk>
2025-01-03 15:45:38 +0000

    Script parallelized with dask; works on 4 local CPUs
    Added 'runid' argument for output file names (back) into FSM2

--- ensemble.py
+++ ensemble.py
@@ -3,34 +3,76 @@ Run model ensemble
 """
 import numpy as np
 import os
+import dask.bag as db
+from dask.distributed import Client
+import dask.delayed
+import time
 
+nCPUs = 4
 Nens = 100    # ensemble size
 Pstd = 0.63   # precipitation multiplier standard deviation
 Tstd = 1.0    # temperature offset standard deviation
 
-# compile fortran code
-os.system('./compil.sh') 
+# function which writes info from the tuple t=(label, Pmlt, Tadd) to 
+# input file nlst_i and returns file name
+def generateInputFile(t):
+    filename = "nlst_"+t[0]
+    nlst = open(filename,'w')
+    print("Writing ", filename)
+    nlst.write('&params \n') 
+    nlst.write('  Pmlt = '+str(t[1])+' \n') 
+    nlst.write('  Tadd = '+str(t[2])+' \n') 
+    nlst.write('/ \n') 
+    nlst.write('&outputs \n') 
+    nlst.write("  runid = '"+t[0]+"_' \n") 
+    nlst.write('/ \n') 
+    nlst.close()
+    return filename
 
-# open loop simulation
-nlst = open('nlst','w')
-nlst.write('&params \n') 
-nlst.write('/ \n') 
-nlst.close()
-os.system('./FSM2 < nlst')
-os.system('mv FSM2out.nc FSM2out_opn.nc')
+def runFSM2(filename):   
+    print("Running ./FSM2 <", filename)
+    os.system('./FSM2 < '+ filename)
+    # return exit code ?
 
-# generate meteorology perturbations
-Pmlt = np.random.lognormal(-0.5*Pstd**2,Pstd,Nens)
-Tadd = np.random.normal(0.,Tstd,Nens)
+if __name__=="__main__":
+    # just for info, print link to show computation
+    client = Client()
+    print(client)
+    print("Click here to see dask dashboard: ", client.dashboard_link)
 
-# run the ensemble and copy outputs
-for n in range(Nens):
+
+    # compile fortran code
+    os.system('./compil.sh') 
+
+    # Start dask bags
+
+    # 100 parallel dask tasks to generate 100 pairs of random numbers
+    # as initital content of the bag
+    get_Pmlt = lambda : np.random.lognormal(-0.5*Pstd**2, Pstd)
+    get_Tadd = lambda : np.random.normal(0., Tstd )
+    tasks = db.range(Nens, npartitions=nCPUs)
+    def getRandom(i):
+        return((str(i), get_Pmlt(), get_Tadd()))
+
+    
+    # process random number pair to argument string of FSM2
+    tasks.map(getRandom).map(generateInputFile).map(runFSM2).compute()
+    """
     nlst = open('nlst','w')
     nlst.write('&params \n') 
-    nlst.write('  Pmlt = '+str(Pmlt[n])+' \n') 
-    nlst.write('  Tadd = '+str(Tadd[n])+' \n') 
     nlst.write('/ \n') 
     nlst.close()
     os.system('./FSM2 < nlst')
-    os.system('mv FSM2out.nc FSM2out_'+str(n).zfill(3)+'.nc')
+    os.system('mv FSM2out.nc FSM2out_opn.nc')
 
+    # run the ensemble and copy outputs
+    for n in range(Nens):
+        nlst = open('nlst','w')
+        nlst.write('&params \n') 
+        nlst.write('  Pmlt = '+str(Pmlt[n])+' \n') 
+        nlst.write('  Tadd = '+str(Tadd[n])+' \n') 
+        nlst.write('/ \n') 
+        nlst.close()
+        os.system('./FSM2 < nlst')
+        os.system('mv FSM2out.nc FSM2out_'+str(n).zfill(3)+'.nc')
+    """
--- src/FSM2_GRID.F90
+++ src/FSM2_GRID.F90
@@ -34,7 +34,8 @@ use PARAMETERS, only: &
   fsnd,              &! Soil sand fraction
   Pmlt,              &! Precipitation multiplier for ensemble generation
   rgr0,              &! Fresh snow grain radius (m)
-  Tadd                ! Temperature offset for ensemble generation (K)
+  Tadd,              &! Temperature offset for ensemble generation (K)
+  runid               ! label for output files
 
 use SOILPROPS, only: &
   b,                 &! Clapp-Hornberger exponent
@@ -45,6 +46,7 @@ use SOILPROPS, only: &
   Vsat                ! Volumetric soil moisture at saturation
 
 implicit none
+
 
 ! Grid dimensions
 integer :: &
@@ -118,7 +120,7 @@ real, allocatable :: &
   VAI(:,:)            ! Vegetation area index
 
 ! NetCDF variables
-character(len=33) :: &
+character(len = 33) :: &
   units               ! Time units
 integer :: &
   dimids(3),         &! x, y, t dimension IDs
@@ -126,7 +128,7 @@ integer :: &
   ncid,              &! Dataset ID
   rec,               &! Record number
   status,            &! Error status
-  time,time0,        &! Time (hours)
+  time, time0,        &! Time (hours)
   varids(5)           ! Variable IDs
 real, allocatable :: &
   x(:),              &! x coordinates
@@ -157,72 +159,72 @@ Dzsnow = (/0.1, 0.2, 0.4/)
 Dzsoil = (/0.1, 0.2, 0.4, 0.8/)
 
 ! Grid dimensions
-status = nf90_open('met/LWdown.nc',nf90_nowrite,ncid)
-status = nf90_inq_dimid(ncid,'lon',dimids(1))
-status = nf90_inq_dimid(ncid,'lat',dimids(2))
-status = nf90_inq_dimid(ncid,'time',dimids(3))
-status = nf90_inquire_dimension(ncid,dimids(1),len=Ncols)
-status = nf90_inquire_dimension(ncid,dimids(2),len=Nrows)
-status = nf90_inquire_dimension(ncid,dimids(3),len=Ntime)
+status = nf90_open('met/LWdown.nc',nf90_nowrite, ncid)
+status = nf90_inq_dimid(ncid, 'lon',dimids(1))
+status = nf90_inq_dimid(ncid, 'lat',dimids(2))
+status = nf90_inq_dimid(ncid, 'time',dimids(3))
+status = nf90_inquire_dimension(ncid, dimids(1), len = Ncols)
+status = nf90_inquire_dimension(ncid, dimids(2), len = Nrows)
+status = nf90_inquire_dimension(ncid, dimids(3), len = Ntime)
 allocate(x(Ncols))
 allocate(y(Nrows))
-status = nf90_inq_varid(ncid,'lon',varids(1))
-status = nf90_inq_varid(ncid,'lat',varids(2))
-status = nf90_inq_varid(ncid,'time',varids(3))
-status = nf90_get_var(ncid,varids(1),x)
-status = nf90_get_var(ncid,varids(2),y)
-status = nf90_get_var(ncid,varids(3),time0,start=(/1/))
-status = nf90_get_att(ncid,varids(3),'units',units)
+status = nf90_inq_varid(ncid, 'lon',varids(1))
+status = nf90_inq_varid(ncid, 'lat',varids(2))
+status = nf90_inq_varid(ncid, 'time',varids(3))
+status = nf90_get_var(ncid, varids(1), x)
+status = nf90_get_var(ncid, varids(2), y)
+status = nf90_get_var(ncid, varids(3), time0, start=(/1/))
+status = nf90_get_att(ncid, varids(3), 'units',units)
 status = nf90_close(ncid)
 
 ! Vegetation characteristics
-allocate(alb0(Ncols,Nrows))
-allocate(VAI(Ncols,Nrows))
-allocate(vegh(Ncols,Nrows))
+allocate(alb0(Ncols, Nrows))
+allocate(VAI(Ncols, Nrows))
+allocate(vegh(Ncols, Nrows))
 alb0 = 0.1
 VAI = 0
 vegh = 0
 
 ! Soil properties
-b = 3.1 + 15.7*fcly - 0.3*fsnd
-hcap_soil = (2.128*fcly + 2.385*fsnd)*1e6 / (fcly + fsnd)
-sathh = 10**(0.17 - 0.63*fcly - 1.58*fsnd)
-Vsat = 0.505 - 0.037*fcly - 0.142*fsnd
+b = 3.1+15.7*fcly-0.3*fsnd
+hcap_soil = (2.128*fcly+2.385*fsnd)*1e6 / (fcly+fsnd)
+sathh = 10**(0.17-0.63*fcly-1.58*fsnd)
+Vsat = 0.505-0.037*fcly-0.142*fsnd
 Vcrit = Vsat*(sathh/3.364)**(1/b)
-hcon_soil = (hcon_air**Vsat) * ((hcon_clay**fcly)*(hcon_sand**(1 - fcly))**(1 - Vsat))
+hcon_soil = (hcon_air**Vsat) * ((hcon_clay**fcly)*(hcon_sand**(1-fcly))**(1-Vsat))
 
 ! Allocate and initialize state variables
-allocate(albs(Ncols,Nrows))
-allocate(Nsnow(Ncols,Nrows))
-allocate(Tsrf(Ncols,Nrows))
-allocate(Dsnw(Nsmax,Ncols,Nrows))
-allocate(Qcan(Ncnpy,Ncols,Nrows))
-allocate(Rgrn(Nsmax,Ncols,Nrows))
-allocate(Sice(Nsmax,Ncols,Nrows))
-allocate(Sliq(Nsmax,Ncols,Nrows))
-allocate(Sveg(Ncnpy,Ncols,Nrows))
-allocate(Tcan(Ncnpy,Ncols,Nrows))
-allocate(Tsnow(Nsmax,Ncols,Nrows))
-allocate(Tsoil(Nsoil,Ncols,Nrows))
-allocate(Tveg(Ncnpy,Ncols,Nrows))
-allocate(Vsmc(Nsoil,Ncols,Nrows))
-inquire(file='start.txt',exist=start_file)
+allocate(albs(Ncols, Nrows))
+allocate(Nsnow(Ncols, Nrows))
+allocate(Tsrf(Ncols, Nrows))
+allocate(Dsnw(Nsmax, Ncols, Nrows))
+allocate(Qcan(Ncnpy, Ncols, Nrows))
+allocate(Rgrn(Nsmax, Ncols, Nrows))
+allocate(Sice(Nsmax, Ncols, Nrows))
+allocate(Sliq(Nsmax, Ncols, Nrows))
+allocate(Sveg(Ncnpy, Ncols, Nrows))
+allocate(Tcan(Ncnpy, Ncols, Nrows))
+allocate(Tsnow(Nsmax, Ncols, Nrows))
+allocate(Tsoil(Nsoil, Ncols, Nrows))
+allocate(Tveg(Ncnpy, Ncols, Nrows))
+allocate(Vsmc(Nsoil, Ncols, Nrows))
+inquire(file='start.txt',exist = start_file)
 if (start_file) then  ! Initialize from file start if it exists
-  open(8,file='start.txt')
-  read(8,*) albs
-  read(8,*) Dsnw
-  read(8,*) Nsnow
-  read(8,*) Qcan
-  read(8,*) Rgrn
-  read(8,*) Sice
-  read(8,*) Sliq
-  read(8,*) Sveg
-  read(8,*) Tcan
-  read(8,*) Tsnow
-  read(8,*) Tsoil
-  read(8,*) Tsrf
-  read(8,*) Tveg
-  read(8,*) Vsmc
+  open(8, file='start.txt')
+  read(8, *) albs
+  read(8, *) Dsnw
+  read(8, *) Nsnow
+  read(8, *) Qcan
+  read(8, *) Rgrn
+  read(8, *) Sice
+  read(8, *) Sliq
+  read(8, *) Sveg
+  read(8, *) Tcan
+  read(8, *) Tsnow
+  read(8, *) Tsoil
+  read(8, *) Tsrf
+  read(8, *) Tveg
+  read(8, *) Vsmc
   close(8)
 else                  ! Cold start
   albs = 0.8
@@ -242,22 +244,22 @@ else                  ! Cold start
 end if
 
 ! Allocate diagnostic output arrays
-allocate(fsnow(Ncols,Nrows))
-allocate(H(Ncols,Nrows))
-allocate(LE(Ncols,Nrows))
-allocate(LWout(Ncols,Nrows))
-allocate(LWsub(Ncols,Nrows))
-allocate(Melt(Ncols,Nrows))
-allocate(Roff(Ncols,Nrows))
-allocate(snd(Ncols,Nrows))
-allocate(snw(Ncols,Nrows))
-allocate(subl(Ncols,Nrows))
-allocate(svg(Ncols,Nrows))
-allocate(SWout(Ncols,Nrows))
-allocate(SWsub(Ncols,Nrows))
-allocate(Tsub(Ncols,Nrows))
-allocate(Usub(Ncols,Nrows))
-allocate(Wflx(Nsmax,Ncols,Nrows))
+allocate(fsnow(Ncols, Nrows))
+allocate(H(Ncols, Nrows))
+allocate(LE(Ncols, Nrows))
+allocate(LWout(Ncols, Nrows))
+allocate(LWsub(Ncols, Nrows))
+allocate(Melt(Ncols, Nrows))
+allocate(Roff(Ncols, Nrows))
+allocate(snd(Ncols, Nrows))
+allocate(snw(Ncols, Nrows))
+allocate(subl(Ncols, Nrows))
+allocate(svg(Ncols, Nrows))
+allocate(SWout(Ncols, Nrows))
+allocate(SWsub(Ncols, Nrows))
+allocate(Tsub(Ncols, Nrows))
+allocate(Usub(Ncols, Nrows))
+allocate(Wflx(Nsmax, Ncols, Nrows))
 
 ! Driving data characteristics
 dt = 3600
@@ -266,112 +268,112 @@ zT = 2
 zU = 10
 
 ! Allocate and read driving data
-allocate(fs(Ncols,Nrows,Ntime))
-allocate(LW(Ncols,Nrows,Ntime))
-allocate(Ps(Ncols,Nrows,Ntime))
-allocate(Qa(Ncols,Nrows,Ntime))
-allocate(Rf(Ncols,Nrows,Ntime))
-allocate(Sf(Ncols,Nrows,Ntime))
-allocate(Sdif(Ncols,Nrows,Ntime))
-allocate(Sdir(Ncols,Nrows,Ntime))
-allocate(Ta(Ncols,Nrows,Ntime))
-allocate(Tc(Ncols,Nrows,Ntime))
-allocate(Ua(Ncols,Nrows,Ntime))
-allocate(trans(Ncols,Nrows))
-call READNC(Ncols,Nrows,Ntime,'LWdown',LW)
-call READNC(Ncols,Nrows,Ntime,'Psurf',Ps)
-call READNC(Ncols,Nrows,Ntime,'RelHum',Qa)
-call READNC(Ncols,Nrows,Ntime,'Precip',Rf)
-call READNC(Ncols,Nrows,Ntime,'SWdown',Sdif)
+allocate(fs(Ncols, Nrows, Ntime))
+allocate(LW(Ncols, Nrows, Ntime))
+allocate(Ps(Ncols, Nrows, Ntime))
+allocate(Qa(Ncols, Nrows, Ntime))
+allocate(Rf(Ncols, Nrows, Ntime))
+allocate(Sf(Ncols, Nrows, Ntime))
+allocate(Sdif(Ncols, Nrows, Ntime))
+allocate(Sdir(Ncols, Nrows, Ntime))
+allocate(Ta(Ncols, Nrows, Ntime))
+allocate(Tc(Ncols, Nrows, Ntime))
+allocate(Ua(Ncols, Nrows, Ntime))
+allocate(trans(Ncols, Nrows))
+call READNC(Ncols, Nrows, Ntime, 'LWdown',LW)
+call READNC(Ncols, Nrows, Ntime, 'Psurf',Ps)
+call READNC(Ncols, Nrows, Ntime, 'RelHum',Qa)
+call READNC(Ncols, Nrows, Ntime, 'Precip',Rf)
+call READNC(Ncols, Nrows, Ntime, 'SWdown',Sdif)
 Sdir(:,:,:) = 0
-call READNC(Ncols,Nrows,Ntime,'Tair',Ta)
-call READNC(Ncols,Nrows,Ntime,'Wind',Ua)
+call READNC(Ncols, Nrows, Ntime, 'Tair',Ta)
+call READNC(Ncols, Nrows, Ntime, 'Wind',Ua)
 trans(:,:) = 0
 
 ! Perturbed driving data
-Ta = Ta + Tadd
+Ta = Ta+Tadd
 Rf = Pmlt*Rf
-Tc = Ta - Tm
-Qa = eps*(e0/Ps)*exp(17.5043*Tc/(241.3 + Tc))*(Qa/100)
-fs = 1 - 0.5*Tc
+Tc = Ta-Tm
+Qa = eps*(e0/Ps)*exp(17.5043*Tc/(241.3+Tc))*(Qa/100)
+fs = 1-0.5*Tc
 where(fs < 0) fs = 0
 where(fs > 1) fs = 1
 Sf = fs*Rf
-Rf = (1 - fs)*Rf
+Rf = (1-fs)*Rf
 
 ! Prepare NetCDF output file
-status = nf90_create('FSM2out.nc',nf90_clobber,ncid)
-status = nf90_def_dim(ncid,'lon',Ncols,dimids(1))
-status = nf90_def_var(ncid,'lon',NF90_REAL,dimids(1),varids(1))
-status = nf90_def_dim(ncid,'lat',Nrows,dimids(2))
-status = nf90_def_var(ncid,'lat',NF90_REAL,dimids(2),varids(2))
-status = nf90_def_dim(ncid,'time',NF90_UNLIMITED,dimids(3))
-status = nf90_def_var(ncid,'time',NF90_REAL,dimids(3),varids(3))
-status = nf90_def_var(ncid,'snc',NF90_REAL,dimids,varids(4))
-status = nf90_def_var(ncid,'snw',NF90_REAL,dimids,varids(5))
-status = nf90_put_att(ncid,varids(3),'units',units)
-status = nf90_put_att(ncid,varids(4),'long_name','snowcover fraction')
-status = nf90_put_att(ncid,varids(4),'units','-')
-status = nf90_put_att(ncid,varids(5),'long_name','mass of snowpack')
-status = nf90_put_att(ncid,varids(5),'units','kg m-2')
+status = nf90_create(trim(runid)//trim('FSM2out.nc'), nf90_clobber, ncid)
+status = nf90_def_dim(ncid, 'lon',Ncols, dimids(1))
+status = nf90_def_var(ncid, 'lon',NF90_REAL, dimids(1), varids(1))
+status = nf90_def_dim(ncid, 'lat',Nrows, dimids(2))
+status = nf90_def_var(ncid, 'lat',NF90_REAL, dimids(2), varids(2))
+status = nf90_def_dim(ncid, 'time',NF90_UNLIMITED, dimids(3))
+status = nf90_def_var(ncid, 'time',NF90_REAL, dimids(3), varids(3))
+status = nf90_def_var(ncid, 'snc',NF90_REAL, dimids, varids(4))
+status = nf90_def_var(ncid, 'snw',NF90_REAL, dimids, varids(5))
+status = nf90_put_att(ncid, varids(3), 'units',units)
+status = nf90_put_att(ncid, varids(4), 'long_name','snowcover fraction')
+status = nf90_put_att(ncid, varids(4), 'units','-')
+status = nf90_put_att(ncid, varids(5), 'long_name','mass of snowpack')
+status = nf90_put_att(ncid, varids(5), 'units','kg m-2')
 status = nf90_enddef(ncid)
-status = nf90_put_var(ncid,varids(1),x)
-status = nf90_put_var(ncid,varids(2),y) 
+status = nf90_put_var(ncid, varids(1), x)
+status = nf90_put_var(ncid, varids(2), y) 
 
 ! Run the model and write daily output
 rec = 1
 do n = 1, Ntime
-  hour = mod(n,24) 
-  time = time0 + n 
+  hour = mod(n, 24) 
+  time = time0+n 
   do i = 1, Nrows
   do j = 1, Ncols
     call FSM2_TIMESTEP(                                                &
                        ! Driving variables                             &
-                       dt,elev,zT,zU,LW(j,i,n),Ps(j,i,n),Qa(j,i,n),    &
-                       Rf(j,i,n),Sdif(j,i,n),Sdir(j,i,n),Sf(j,i,n),    &
-                       Ta(j,i,n),trans(j,i),Ua(j,i,n),                 &
+                       dt, elev, zT, zU, LW(j, i, n), Ps(j, i, n), Qa(j, i, n),    &
+                       Rf(j, i, n), Sdif(j, i, n), Sdir(j, i, n), Sf(j, i, n),    &
+                       Ta(j, i, n), trans(j, i), Ua(j, i, n),                 &
                        ! Vegetation characteristics                    &
-                       alb0(j,i),vegh(j,i),VAI(j,i),                   &
+                       alb0(j, i), vegh(j, i), VAI(j, i),                   &
                        ! State variables                               &
-                       albs(j,i),Tsrf(j,i),Dsnw(:,j,i),Nsnow(j,i),     &
-                       Qcan(:,j,i),Rgrn(:,j,i),Sice(:,j,i),            &
-                       Sliq(:,j,i),Sveg(:,j,i),Tcan(:,j,i),            &
-                       Tsnow(:,j,i),Tsoil(:,j,i),Tveg(:,j,i),          &
-                       Vsmc(:,j,i),                                    &
+                       albs(j, i), Tsrf(j, i), Dsnw(:,j, i), Nsnow(j, i),     &
+                       Qcan(:,j, i), Rgrn(:,j, i), Sice(:,j, i),            &
+                       Sliq(:,j, i), Sveg(:,j, i), Tcan(:,j, i),            &
+                       Tsnow(:,j, i), Tsoil(:,j, i), Tveg(:,j, i),          &
+                       Vsmc(:,j, i),                                    &
                        ! Diagnostics                                   &
-                       fsnow(j,i),H(j,i),LE(j,i),LWout(j,i),           &
-                       LWsub(j,i),Melt(j,i),Roff(j,i),snd(j,i),        &
-                       snw(j,i),subl(j,i),svg(j,i),SWout(j,i),         &
-                       SWsub(j,i),Tsub(j,i),Usub(j,i),Wflx(:,j,i)      )  
+                       fsnow(j, i), H(j, i), LE(j, i), LWout(j, i),           &
+                       LWsub(j, i), Melt(j, i), Roff(j, i), snd(j, i),        &
+                       snw(j, i), subl(j, i), svg(j, i), SWout(j, i),         &
+                       SWsub(j, i), Tsub(j, i), Usub(j, i), Wflx(:,j, i)      )  
   end do
   end do
-  if (hour==12) then
-    status = nf90_put_var(ncid,varids(3),time,start=(/rec/))
-    status = nf90_put_var(ncid,varids(4),fsnow,start=(/1,1,rec/),  &
-                          count=(/Ncols,Nrows,1/))
-    status = nf90_put_var(ncid,varids(5),snw,start=(/1,1,rec/),    &
-                          count=(/Ncols,Nrows,1/))
-    rec = rec + 1
+  if (hour == 12) then
+    status = nf90_put_var(ncid, varids(3), time, start=(/rec/))
+    status = nf90_put_var(ncid, varids(4), fsnow, start=(/1, 1, rec/),  &
+                          count=(/Ncols, Nrows, 1/))
+    status = nf90_put_var(ncid, varids(5), snw, start=(/1, 1, rec/),    &
+                          count=(/Ncols, Nrows, 1/))
+    rec = rec+1
   end if
 end do
-status=nf90_close(ncid)
+status = nf90_close(ncid)
 
 ! Write out state variables at end of run
-open(8,file='dump.txt')
-write(8,*) albs
-write(8,*) Dsnw
-write(8,*) Nsnow
-write(8,*) Qcan
-write(8,*) Rgrn
-write(8,*) Sice
-write(8,*) Sliq
-write(8,*) Sveg
-write(8,*) Tcan
-write(8,*) Tsnow
-write(8,*) Tsoil
-write(8,*) Tsrf
-write(8,*) Tveg
-write(8,*) Vsmc
+open(8, file='dump.txt')
+write(8, *) albs
+write(8, *) Dsnw
+write(8, *) Nsnow
+write(8, *) Qcan
+write(8, *) Rgrn
+write(8, *) Sice
+write(8, *) Sliq
+write(8, *) Sveg
+write(8, *) Tcan
+write(8, *) Tsnow
+write(8, *) Tsoil
+write(8, *) Tsrf
+write(8, *) Tveg
+write(8, *) Vsmc
 close(8)
 
 end program FSM2_GRID
