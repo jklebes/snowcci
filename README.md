f4cf929b7cb326c6eebe77c6400f9e8418c532e2
jklebes <jason.klebes@ed.ac.uk>
2025-01-06 10:00:08 +0000

    Least-intervention alternative with gnu-parallel bash command

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 ensemble.py uses compil.sh to compile the fortran code in src and runs an ensemble of simulation with inputs from met/.
 
-Notes:
+Version using gnu-parallel bash command
 
+Notes:
 - install fortran netcdf, on ubuntu ``apt install libnetcdff-dev``
-- Install a python ``<``3.11 environment with dask
-- Currently fortran CPU and memory activity does not show up on dask taskboard
+- install gnu-parallel with `conda install parallel -c conda-forge`
+- edit nCPUs in script ensemble.py
--- ensemble.py
+++ ensemble.py
@@ -1,59 +1,38 @@
 """
-Run model ensemble
+Run model ensemble 
 """
+
+# minimal change version using gnu-parallel bash command.
+# To use: `conda install parallel -c conda-forge` or 
+# distribution-specific gnu-parallel install
+
 import numpy as np
 import os
-import dask.bag as db
-from dask.distributed import Client
-import dask.delayed
-import time
 
-nCPUs = 4
+nCPUs = 2
 Nens = 100    # ensemble size
 Pstd = 0.63   # precipitation multiplier standard deviation
 Tstd = 1.0    # temperature offset standard deviation
 
-# function to transform int to tuple (label, Pmlt, Tadd)
-def getRandom(i):
-     return((str(i), np.random.lognormal(-0.5*Pstd**2, Pstd), np.random.normal(0., Tstd) ) )
+# compile fortran code
+os.system('./compil.sh') 
 
-# function which writes info from the tuple t=(label, Pmlt, Tadd) to 
-# input file nlst_i and returns file name
-def generateInputFile(t):
-    filename = "nlst_"+t[0]
-    nlst = open(filename,'w')
-    print("Writing ", filename)
+# generate meteorology perturbations
+Pmlt = np.random.lognormal(-0.5*Pstd**2,Pstd,Nens)
+Tadd = np.random.normal(0.,Tstd,Nens)
+
+# write the different input files nlst
+for n in range(Nens):
+    nlst = open('nlst_'+str(n),'w')
     nlst.write('&params \n') 
-    nlst.write('  Pmlt = '+str(t[1])+' \n') 
-    nlst.write('  Tadd = '+str(t[2])+' \n') 
+    nlst.write('  Pmlt = '+str(Pmlt[n])+' \n') 
+    nlst.write('  Tadd = '+str(Tadd[n])+' \n') 
     nlst.write('/ \n') 
     nlst.write('&outputs \n') 
-    nlst.write("  runid = '"+t[0]+"_' \n") 
+    nlst.write('  runid = '+str(n)+'_ \n') 
     nlst.write('/ \n') 
     nlst.close()
-    return filename
 
-# run the simulation given file name
-def runFSM2(filename):   
-    print("Running ./FSM2 <", filename)
-    os.system('./FSM2 < '+ filename)
-    # return exit code ?
+# process input files by launching parallel ./FSM2 commands 
+os.system("ls nlst_* | parallel --jobs "+str(nCPUs)+" '(cat {} | ./FSM2 )'")
 
-
-if __name__=="__main__":
-    # just for info, print link to show computation
-    client = Client()
-    print(client)
-    print("Click here to see dask dashboard: ", client.dashboard_link)
-
-    # compile fortran code
-    os.system('./compil.sh') 
-    
-    # create Bag containing ints 0..99
-    tasks = db.range(Nens, npartitions=nCPUs)
-
-    # run the sequence of steps: 
-    # generate random values Pmlt and Tadd
-    # write input file
-    # run fortran exectuable
-    tasks.map(getRandom).map(generateInputFile).map(runFSM2).compute()
